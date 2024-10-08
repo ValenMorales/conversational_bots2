0d0fc381a02bd9642f4bd270a822ee50400e75e1
ValenMorales <valentina.morales@kommit.co>
2024-10-08 17:21:14 -0500

    changes in telegram bot structure to manage commands given in the inicialization

--- main.rb
+++ main.rb
@@ -9,13 +9,31 @@ TELEGRAM_BOT_TOKEN = ENV['TELEGRAM_TOKEN']
 DISCORD_BOT_TOKEN = ENV['DISCORD_TOKEN']
 db_connection = ENV['DB_HOST']
 
-# Initialize Telegram and Discord bots with their respective tokens and a shared database connection
-telegram_bot = TelegramBot::WebAvailability.new(TELEGRAM_BOT_TOKEN, db_connection)
-discord_bot = DiscordBot::WebAvailability.new(DISCORD_BOT_TOKEN, db_connection)
+START = 'Hello! Use /add_website to add a new website.'
+ADD_WEBSITE = 'Please send the URL of the website you want to add.'
+WEBSITE_ADDED = 'Thanks! The website has been added. You will be notified if the domain is down'
+INVALID = 'Invalid URL. Please enter a valid website.'
+INSTRUCTION = 'Send /add_website to add a website.'
+
+commands = {
+  "start" => {
+    description: "/start",
+    message: START
+  },
+  "add_website" => {
+    description: "/add_website",
+    message: ADD_WEBSITE
+  }
+}
+
+telegram_bot = TelegramBot::WebAvailability.new(TELEGRAM_BOT_TOKEN, db_connection, commands)
+
+# Inicia el bot
+discord_bot = DiscordBot::WebAvailability.new(DISCORD_BOT_TOKEN, db_connection, commands)
 
 # Create threads to run both bots concurrently
 threads = []
-threads << Thread.new { telegram_bot.execute }
+threads << Thread.new { telegram_bot.start }
 threads << Thread.new { discord_bot.execute }
 
 # Wait for both threads to finish execution
