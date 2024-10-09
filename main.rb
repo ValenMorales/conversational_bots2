d4f180285154ab56c5a21cd092f7c121883e81f4
HumanPseudo <cadalagu@hotmail.com>
2024-10-09 10:58:59 -0500

    add generate_commands function

--- main.rb
+++ main.rb
@@ -11,7 +11,7 @@ db_connection = ENV['DB_HOST']
 
 START = 'Hello! Use /add_website to add a new website.'
 ADD_WEBSITE = 'Please send the URL of the website you want to add.'
-WEBSITE_ADDED = 'Thanks! The website has been added. You will be notified if the domain is down'
+WEBSITE_ADDED = 'Thanks! The website has been added. You will be notified if the domain is down.'
 INVALID = 'Invalid URL. Please enter a valid website.'
 INSTRUCTION = 'Send /add_website to add a website.'
 
@@ -26,41 +26,83 @@ end
 
 def save_website(event, message, user, bot_instance)
   owner = user.is_a?(Integer) ? user : user.id
-  
   config = { connection: db_connection, owner: owner, url: message }
   Utils::AddReview.new(config).execute
 end
 
+# Function to generate the commands hash from user input
+def generate_commands
+  commands = {}
+  puts "Welcome to the Command Generator. Enter the command details below."
 
-commands = {
-  "start" => {
-    description: "/start",
-    message: START
-  },
-  "add_website" => {
-    description: "/add_website",
-    message: ADD_WEBSITE,
-  },
-  "/check_status" => {
-    description: "/check_status",
-    type:"discord",
-    action: Proc.new { |event| 
-      puts "Chequeando el estado del sistema..."
-      event.user.pm("El sistema está funcionando correctamente.")
-    }
-  },
-}
+  loop do
+    puts "\nEnter command name (or type 'exit' to finish):"
+    command_name = gets.chomp.downcase
+    break if command_name == 'exit'
 
+    puts "Enter description for '#{command_name}':"
+    description = gets.chomp
 
-telegram_bot = TelegramBot::WebAvailability.new(TELEGRAM_BOT_TOKEN, commands, custom_handler)
+    puts "Enter message for '#{command_name}' (leave blank if not applicable):"
+    message = gets.chomp
 
-# Inicia el bot
-discord_bot = DiscordBot::WebAvailability.new(DISCORD_BOT_TOKEN, commands, custom_handler)
+    puts "Is this a Discord-specific command? (yes/no)"
+    is_discord = gets.chomp.downcase == 'yes'
+
+    # Action block, optional for commands with specific actions
+    action = nil
+    if is_discord
+      puts "Do you want to add a custom action for this command? (yes/no)"
+      add_action = gets.chomp.downcase == 'yes'
+      if add_action
+        puts "Custom action will be added. Example actions will be included in your bot logic."
+        action = Proc.new { |event|
+          puts "Executing custom action for #{command_name}..."
+          event.user.pm("#{command_name.capitalize} action executed!")
+        }
+      end
+    end
+
+    # Building the command hash
+    commands[command_name] = {
+      description: "/#{command_name}",
+      message: message.empty? ? nil : message,
+      type: is_discord ? "discord" : nil,
+      action: action
+    }.compact
+
+    puts "Command '#{command_name}' added!"
+  end
+
+  commands
+end
+
+# Generate the commands hash by calling the function
+generated_commands = generate_commands
+puts "\nGenerated Commands JSON:"
+puts generated_commands
+
+# Instantiate the bots
+telegram_bot = TelegramBot::WebAvailability.new(TELEGRAM_BOT_TOKEN, generated_commands, custom_handler)
+discord_bot = DiscordBot::WebAvailability.new(DISCORD_BOT_TOKEN, generated_commands, custom_handler)
 
 # Create threads to run both bots concurrently
 threads = []
 threads << Thread.new { telegram_bot.start }
 threads << Thread.new { discord_bot.start }
 
+# Monitor for 'exit' input to gracefully exit the program
+Thread.new do
+  loop do
+    input = gets.chomp
+    if input.downcase == 'exit'
+      puts 'Exiting the program...'
+      exit 0  # Exit the program immediately
+    end
+  end
+end
+
 # Wait for both threads to finish execution
 threads.each(&:join)
+
+puts 'Bots stopped, exiting program.'
